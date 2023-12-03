623bea0bd68ae287bb5d4f73410fb9c648280351
kbrosal <140308832+kbrosal@users.noreply.github.com>
2023-12-03 10:48:19 +0800

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="deproject1.cpaxrao0tqen.ap-southeast-1.rds.amazonaws.com",
+    database="postgres",
+    user="dbproject1",
+    password="kerlydata",
+    port = "5432")
+
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE restaurants_LA(
+    restaurants_name VARCHAR PRIMARY KEY,
+    phone_number VARCHAR(15),
+    street_address VARCHAR(30),
+    locality VARCHAR(30),
+    website VARCHAR,
+    menu_link VARCHAR,
+    categories VARCHAR(50),
+    sub_categories VARCHAR(50),
+    internal_rating FLOAT,
+    number_of_reviews INT,
+    TA_ratings FLOAT,
+    TA_rating_count INT,
+    years_in_business VARCHAR(15),
+    top_comment VARCHAR                            
+)
+""")
+
+file_path = 'C:/Users/username/OneDrive/Desktop/DEProjects/restaurantsLA.csv'
+
+copy_sql = """
+COPY restaurants_la FROM stdin WITH CSV HEADER
+DELIMITER as ','
+"""
+
+try:
+    with open(file_path, 'r') as f:
+        cur.copy_expert(copy_sql, f)
+    conn.commit()
+    print("Data loaded successfully")
+except Exception as e:
+    conn.rollback()
+    print("Error: ", e)
+finally:
+    cur.close()
+    conn.close()
